42c42c294c6de4ee6b6959036db4b1d3c6db7990
Collins Mutai <collinsfrontend@gmail.com>
2023-08-13 17:28:36 +0300

    res

--- backend/controllers/tenantsController.js
+++ backend/controllers/tenantsController.js
@@ -13,40 +13,11 @@ exports.createTenant = async (req, res, next) => {
 
   const errors = validationResult(req);
 
-  // if (!errors.isEmpty()) {
-  // const tenants = await Tenant.find({ landlordId: req.landlord._id }).sort({
-  //   apt: "asc",
-  // });
-  // return res.redirect("/tenants");
-  // res.render("tenants", {
-  //   path: "/tenants",
-  //   tenants: tenants,
-  //   tenant: "",
-  //   errorMessage: errors.array()[0].msg,
-  //   validationErrors: errors.array(),
-  //   editing: false,
-  // });
-  // }
-  try {
-    //  const tenantt = await Tenant.findById(tenantId);
-    const tenants = await Tenant.find({ landlordId: req.landlord._id }).sort({
-      apt: "asc",
-    });
-    if (!errors.isEmpty()) {
-      return res.render("tenants", {
-        path: "/tenants",
-        tenants: tenants,
-        tenant: "",
-        oldInput: {
-          name: name,
-          apt: apt,
-        },
+  if (!errors.isEmpty()) {
+    return res.redirect("/tenants");
+  }
 
-        errorMessage: errors.array()[0].msg,
-        validationErrors: errors.array(),
-        editing: false,
-      });
-    }
+  try {
     const tenant = new Tenant({
       name: name,
       apt: apt,
@@ -73,7 +44,6 @@ exports.getTenants = async (req, res, next) => {
       errorMessage: "",
       validationErrors: [],
       editing: false,
-      oldInput: ''
     });
   } catch (err) {
     const error = new Error(err);
@@ -174,10 +144,12 @@ exports.deleteTenant = async (req, res, next) => {
 
 exports.getEditTenant = async (req, res, next) => {
   const tenantId = req.params.tenantId;
-  console.log(tenantId);
+
   try {
     const tenant = await Tenant.findById(tenantId);
-    const tenants = await Tenant.find().sort({ apt: "asc" });
+    const tenants = await Tenant.find({ landlordId: req.landlord._id }).sort({
+      apt: "asc",
+    });
     if (!tenant) {
       return res.redirect("/tenants");
     }
@@ -185,7 +157,7 @@ exports.getEditTenant = async (req, res, next) => {
     return res.render("tenants", {
       path: "/tenants",
       editing: true,
-      oldInput: '',
+
       tenant: tenant,
       tenants: tenants,
       errorMessage: "",
@@ -202,26 +174,13 @@ exports.postEditTenant = async (req, res, next) => {
   const name = req.body.name;
   const apt = req.body.apt;
   const tenantId = req.params.tenantId;
-  console.log(tenantId);
 
   const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.redirect("/tenants");
+  }
   try {
     const tenant = await Tenant.findById(tenantId);
-    const tenants = await Tenant.find({ landlordId: req.landlord._id }).sort({
-      apt: "asc",
-    });
-    if (!errors.isEmpty()) {
-      return res.render("tenants", {
-        path: "/tenants",
-        tenants: tenants,
-        tenant: tenant,
-        errorMessage: errors.array()[0].msg,
-        validationErrors: errors.array(),
-        editing: true,
-        oldInput: "",
-      });
-    }
-
     tenant.name = name;
     tenant.apt = apt;
     console.log(tenant);

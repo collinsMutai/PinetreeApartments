a9ee8aea33138e3b58ce56c4ac313fa2780c818b
Collins Mutai <collinsfrontend@gmail.com>
2023-12-19 12:24:05 +0300

    water bill

--- backend/controllers/tenantsController.js
+++ backend/controllers/tenantsController.js
@@ -14,7 +14,6 @@ exports.createTenant = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    
     req.flash("error", "Apt# already assigned, please add another one.");
     return res.redirect("/tenants");
   }
@@ -43,7 +42,7 @@ exports.getTenants = async (req, res, next) => {
       path: "/tenants",
       tenants: tenants,
       tenant: "",
-      errorMessage: req.flash('error'),
+      errorMessage: req.flash("error"),
       validationErrors: [],
       editing: false,
     });
@@ -96,7 +95,7 @@ exports.getInvoice = async (req, res, next) => {
     });
     pdfDoc
       .fontSize(16)
-      .text("Invoice", {
+      .text("Rent Invoice", {
         underline: true,
       })
       .moveDown(2);
@@ -107,6 +106,96 @@ exports.getInvoice = async (req, res, next) => {
       .moveDown(0.5);
 
     pdfDoc.fontSize(13).text("Rent Paid - 5000").moveDown(0.5);
+
+    pdfDoc.text("---------------").moveDown(2);
+
+    pdfDoc.fontSize(12).text("Thank you for your business.").moveDown(0.5);
+
+    pdfDoc
+      .fontSize(11)
+      .text("Contact info - 0797759858", {
+        underline: true,
+      })
+      .moveDown(0.5);
+
+    pdfDoc.end();
+  } catch (err) {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  }
+};
+
+exports.getWaterInvoice = async (req, res, next) => {
+  const apt = req.body.apt;
+  const amount = req.body.amount;
+
+  console.log(apt, amount);
+
+  const errors = validationResult(req);
+
+  console.log(errors);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("tenants", {
+      path: "/tenants",
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
+
+  try {
+    const user = await Tenant.findOne({ apt: apt });
+    console.log(user);
+
+    const result = { apt: user.apt, tenant: user.name };
+
+    const d = new Date();
+    let year = d.getFullYear().toString();
+    let month = d.getMonth() + 1;
+    let day = d.getDate().toString();
+    let paidDate = day + month.toString() + year;
+
+    const invoiceName = "invoice-" + paidDate + result.tenant + ".pdf";
+    const invoicePath = path.join("data", "invoices", invoiceName);
+
+    const pdfDoc = new PDFDocument({ size: "A6" });
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'download; filename="' + invoiceName + '"'
+    );
+    // pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc
+      .image("images/logo.png", {
+        fit: [150, 150],
+        align: "center",
+        valign: "top",
+      })
+      .moveDown(1.5);
+    const date = new Date();
+
+    const formatedDate = date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+    pdfDoc
+      .fontSize(16)
+      .text("Water invoice", {
+        underline: true,
+      })
+      .moveDown(2);
+
+    pdfDoc
+      .fontSize(14)
+      .text(`Apt#${result.apt} - Date ${formatedDate}`)
+      .moveDown(0.5);
+
+    pdfDoc.fontSize(13).text(`Water payment amount Kshs ${amount}`).moveDown(0.5);
 
     pdfDoc.text("---------------").moveDown(2);
 
@@ -162,7 +251,7 @@ exports.getEditTenant = async (req, res, next) => {
 
       tenant: tenant,
       tenants: tenants,
-      
+
       errorMessage: req.flash("error"),
       validationErrors: [],
     });
@@ -179,7 +268,7 @@ exports.postEditTenant = async (req, res, next) => {
   const tenantId = req.params.tenantId;
 
   const errors = validationResult(req);
-  
+
   if (!errors.isEmpty()) {
     req.flash("error", "Apt# already assigned, please add another one.");
     return res.redirect("/tenants");
--- backend/routes/tenantsRoutes.js
+++ backend/routes/tenantsRoutes.js
@@ -3,21 +3,24 @@ const { check, body } = require("express-validator");
 const Tenant = require("../model/tenantsSchema");
 const router = express.Router();
 
-const isAuth = require("../middleware/is-auth")
+const isAuth = require("../middleware/is-auth");
 
 const tenantsController = require("../controllers/tenantsController");
 
 router.post(
-  "/tenants", isAuth,
+  "/tenants",
+  isAuth,
   [
     check("apt").custom((value, { req }) => {
-      return Tenant.findOne({ apt: value, landlordId: req.landlord._id }).then((userDoc) => {
-        if (userDoc) {
-          return Promise.reject(
-            "Apt# already assigned, please add another one."
-          );
+      return Tenant.findOne({ apt: value, landlordId: req.landlord._id }).then(
+        (userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Apt# already assigned, please add another one."
+            );
+          }
         }
-      });
+      );
     }),
   ],
   tenantsController.createTenant
@@ -26,6 +29,16 @@ router.post(
 router.get("/tenants", isAuth, tenantsController.getTenants);
 
 router.get("/tenants/:tenantId", isAuth, tenantsController.getInvoice);
+
+// router.post(
+//   "/tenants/water",
+//   [
+//     body("apt", "Apt# is required").not().isEmpty(),
+//     body("amount", "Amount is required").not().isEmpty(),
+//   ],
+//   tenantsController.getWaterInvoice
+// );
+router.post("/tenants/water", tenantsController.getWaterInvoice);
 
 router.post("/tenants/:tenantId", isAuth, tenantsController.deleteTenant);
 
